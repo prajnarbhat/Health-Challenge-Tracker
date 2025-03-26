2f355d48debe5a4e33763e1216dd7894d45ea01d
prajnarbhat <rbhatprajna@gmail.com>
2025-03-26 15:29:44 +0530

    Designing home page with styling

---
+++ src/components/Home.js
@@ -0,0 +1,32 @@
+const Home = () => {
+    return (
+        <>
+            <form className="input-form">
+                <div className="form-element" style={{marginLeft: "340px"}}>
+                    <label>User Name: </label> 
+                    <input type="text" name="name" style={{outline:"none"}}/>
+                </div>
+                <br></br>
+                <div class="form-ele">
+                <div className="form-element">
+                    <label> Workout Type: </label>
+                    <select>
+                        <option value="Cycling">Cycling</option>
+                        <option value="Running">Running</option>
+                        <option value="Yoga">Yoga</option>
+                        <option value="Swimming">Swimming</option>
+                    </select>
+                </div>
+                <br></br>
+                <div className="form-element">
+                    <label> Workout Minutes: </label>
+                        <input type="number"/>
+                </div>
+                </div>
+            </form>
+            
+        </>
+    )
+}
+
+export default Home;
