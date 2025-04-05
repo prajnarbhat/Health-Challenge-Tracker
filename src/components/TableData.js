6a347f78a258aa86efcbf335c9a681ddf85d674f
prajnarbhat <rbhatprajna@gmail.com>
2025-04-05 13:22:37 +0530

    Filter functionality

--- src/components/TableData.js
+++ src/components/TableData.js
@@ -4,18 +4,76 @@ import { DataContext } from "./DataContext";
 const TableData = () => {
     const { data } = useContext(DataContext);
     const [page, setPage] = useState(1);
+    let [searchText, setSearchtext] = useState("")
     const itemsPerPage = 5;
 
-    
     const paginatedData = data.slice((page - 1) * itemsPerPage, page * itemsPerPage);
+
+    const [userData, setData] = useState(paginatedData)
+
+    const [workoutValue, setWorkoutvalue] = useState('')
+
+    const workoutData = [ "Cycling", "Running", "Yoga", "Swimming" ];
 
     const setPageHandler = (selectedPage) => {
         if( selectedPage >= 1 && selectedPage <= Math.ceil(data.length/itemsPerPage) && selectedPage !== page )
         setPage(selectedPage)
     }
+
+    const setSearchhandler = (e) => {
+        searchText = e.target.value;
+        setSearchtext(searchText)
+
+        const filterDataWithNames = paginatedData.filter(item => {
+            return item.userName.toLowerCase().includes(searchText.toLowerCase());
+        })
+
+        setData(filterDataWithNames)
+        
+    }
+
+    const displayWorkoutTypes = (e) => {
+        const value = e.target.value;
+        setWorkoutvalue(value)
+
+        const getWorkoutType = paginatedData.map(item =>
+            item.workouts.map(wtype => 
+                wtype.workoutType
+            ).join(",")
+        ).join(",")
+        console.log(getWorkoutType)
+
+        const arrayOfWorkoutType = getWorkoutType.split(',')
+        console.log(arrayOfWorkoutType)
+
+        const filterDataWithWorkoutType = paginatedData.filter(item =>
+            item.workouts.some(wtype =>
+                wtype.workoutType.toLowerCase().includes(value.toLowerCase())
+            )
+        );
+
+        console.log(filterDataWithWorkoutType)
+        setData(filterDataWithWorkoutType)
+
+        setWorkoutvalue("")
+
+ 
+    }
+
+    
     return (
         <>
             <h2>Workout Summary</h2>
+            <div>
+                <input type="text" value={searchText} onChange={setSearchhandler}/>
+            </div>
+            <div>
+                <input list="data" onChange={(e) => displayWorkoutTypes(e)}/>
+                <datalist id="data">
+                    {workoutData.map(wdata => <option> {wdata} </option>)}
+                </datalist>
+                
+            </div>
             <table border="1">
                 <thead>
                     <tr>
@@ -26,7 +84,7 @@ const TableData = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    {paginatedData.map((user, index) => (
+                    {userData.map((user, index) => (
                         <tr key={index}>
                             <td>{user.userName}</td>
                             <td>{user.workouts.map(w => w.workoutType).join(", ")}</td>
